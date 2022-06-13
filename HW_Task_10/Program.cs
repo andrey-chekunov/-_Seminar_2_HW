fb4a94cad9430b2540d564dd11a2a584dab94ed0
andrey-chekunov <79117325352@yandex.ru>
2022-06-13 14:07:01 +0300

    Resolved Task_10

---
+++ HW_Task_10/Program.cs
@@ -0,0 +1,10 @@
+﻿/* Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+456 -> 5
+782 -> 8
+918 -> 1 */
+
+Console.Write("Input your three-digit number: ");
+int threeDigitNumber = Convert.ToInt32(Console.Read());
+int ab = threeDigitNumber % 100;
+int b = ab / 10;
+Console.Write("Second number in your three-digit number is: " + b);
